113e5b8b6aae61743a2592ac6daf42754d63165c
recepcigdem <recepcigdem24@gmail.com>
2020-12-18 16:30:37 +0300

    first commit

---
+++ CrudOperations/CrudOperations/Migrations/202012171447042_UserEntity.cs
@@ -0,0 +1,29 @@
+namespace CrudOperations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserEntity : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
